0ee5b314ad6dfafae1f8dcee965cf7bffa8ec0c8
shumpd <fujijish@gmail.com>
2019-03-26 22:24:33 +0900

    add

---
+++ 98_C.py
@@ -0,0 +1,14 @@
+N = int(input())
+S = input()
+
+cost = S.count("E")
+ans = S.count("E")
+
+for s in S:
+    if s == "E":
+        cost -= 1
+        ans = min(ans, cost)
+    else:
+        cost += 1
+
+print(ans)
