86b2a248fb1eebd66986b107bc69990828f85dfc
shumpd <fujijish@gmail.com>
2019-04-02 20:22:01 +0900

    add

--- 87_C.py
+++ 87_C.py
@@ -1,15 +1,9 @@
 N = int(input())
 A1 = list(map(int, input().split()))
 A2 = list(map(int, input().split()))
-sum = 0
+ans = 0
  
-for i in range(len(A1)-1):
-    temp = 0
-    for j, (k, l) in enumerate(zip(A1[1:], A2[:-1])):
-        if j >= i:
-            temp += l
-        else: 
-            temp += k
-    sum = max(sum, temp)
+for i in range(N):
+    ans = max(ans, sum(A1[0:i+1] + A2[i:]))
  
-print(sum + A1[0] + A2[-1])
+print(ans)
