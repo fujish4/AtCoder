c187a3b847541910d7770c3ad9baff2c3218baa0
shumpd <fujijish@gmail.com>
2019-04-23 23:37:12 +0900

    add

---
+++ 117_C.py
@@ -0,0 +1,10 @@
+n, m = map(int, input().split())
+x = sorted(list(map(int, input().split())))
+diff = []
+
+for (i, j) in zip(x, x[1:]):
+    diff.append(j-i)
+
+diff.sort(reverse=True)
+print(sum(diff[n-1:]))
+    
